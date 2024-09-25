7774f19fd824f2564960cedff40af8c6bb9ddebb
dpw980 <dpw982@alumni.ku.dk>
2024-09-25 14:30:21 +0200

    Binsearch

--- src/id_query_binsort.c
+++ src/id_query_binsort.c
@@ -14,6 +14,15 @@ struct indexed_data {
   int n;
 };
 
+int compare_index(const void *a, const void *b) {
+  const struct indexed_record *rec_a = (const struct indexed_record *)a;
+  const struct indexed_record *rec_b = (const struct indexed_record *)b;
+  
+  if(rec_a->osm_id > rec_b->osm_id) {return 1;};
+  if(rec_a->osm_id < rec_b->osm_id) {return -1;};
+  return 0;
+}
+
 struct indexed_data* mk_indexed(struct record* rs, int n) {
   struct indexed_data* data = malloc(sizeof(struct indexed_data));
   data->irs = malloc(n * sizeof(struct indexed_record));
@@ -23,6 +32,7 @@ struct indexed_data* mk_indexed(struct record* rs, int n) {
     data->irs[i].osm_id = rs[i].osm_id;
     data->irs[i].record = &rs[i];
   }
+  qsort(data->irs, n, sizeof(struct indexed_record), compare_index);
   return data;
 }
 
@@ -30,13 +40,18 @@ void free_indexed(struct indexed_data* data) {
   free(data);
 }
 
-const struct record* lookup_indexed(struct indexed_data *data, int64_t needle) {
-  int64_t current_record = 0;
-  while(current_record < data->n) {
-    if(data->irs[current_record].osm_id == needle) {
-        return data->irs[current_record].record;
+const struct record* lookup_binsearch(struct indexed_data *data, int64_t needle) {
+  int64_t low = 0;
+  int64_t high = data->n-1;
+  while(low <= high) {
+    int64_t mid = (low + high) / 2;
+    if(data->irs[mid].osm_id == needle) {
+      return data->irs[mid].record;
+    } else if (data->irs[mid].osm_id < needle) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
     }
-    current_record++;
   }
   return NULL;
 }
@@ -45,5 +60,5 @@ int main(int argc, char** argv) {
   return id_query_loop(argc, argv,
                     (mk_index_fn)mk_indexed,
                     (free_index_fn)free_indexed,
-                    (lookup_fn)lookup_indexed);
+                    (lookup_fn)lookup_binsearch);
 }
